7c8ecf38b59b3429685800b5bd7e50cdd8d44bd0
Alex Poklonsky <jispar@turbanov.ru>
2022-08-27 22:31:09 +0300

    Add another one java gradle project.

---
+++ DeadProjects/mill-factory/src/main/java/com/velikiyprikalel/millfactory/MillFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.velikiyprikalel.millfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillFactoryApplication.class, args);
+	}
+
+}
---
+++ DeadProjects/mill-factory/src/test/java/com/velikiyprikalel/millfactory/MillFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velikiyprikalel.millfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
